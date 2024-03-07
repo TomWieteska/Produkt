ac58c0e82268de99e162b06d274ac2d3640a1648
Tomasz Wieteska <tomasz.wieteska@elektrykplock.edu.pl>
2024-03-07 15:23:44 +0100

    Add project files.

---
+++ Produkt/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Produkty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Produkt _produkt = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            PrzygotujWiazanie();
+        }
+
+        private void PrzygotujWiazanie()
+        {
+            _produkt = new Produkt("cuk-1/P", "Cukierek", 12, "Płock");
+            grdProdukt.DataContext = _produkt;
+        }
+    }
+}
